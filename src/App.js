1f0803fd2ba1bd28d585ba49597efa0c1f13113b
Giuseppe Palazzo <mrgiuseppepalazzo@gmail.com>
2026-09-17 09:53:26 +0200

    Fine tune loading subtitle position

--- src/App.js
+++ src/App.js
@@ -6501,7 +6501,7 @@ function App() {
           />
           <div
             className="stablr-loader-fade-in stablr-loader-fade-in--brand"
-            style={{ marginTop: "-12px", fontSize: "11px", fontWeight: 700, letterSpacing: "0.16em" }}
+            style={{ marginTop: "-8px", fontSize: "11px", fontWeight: 700, letterSpacing: "0.16em" }}
           >
             GOLF SCORE
           </div>
@@ -6842,7 +6842,7 @@ function App() {
           />
           <div
             className="stablr-loader-fade-in stablr-loader-fade-in--brand"
-            style={{ marginTop: "-12px", fontSize: "11px", fontWeight: 700, letterSpacing: "0.16em" }}
+            style={{ marginTop: "-8px", fontSize: "11px", fontWeight: 700, letterSpacing: "0.16em" }}
           >
             GOLF SCORE
           </div>
